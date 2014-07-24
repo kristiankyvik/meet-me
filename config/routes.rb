99a249f63a5d7b5b250f484298fb81f13a8ce1b1
Kristian Ruiz Kyvik <kristianruizkyvik@gmail.com>
2014-07-24 16:29:13 +0200

    added resource user

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :locations do
+  resources :locations, :users do
       resources :visits
   end
-  
+
   root 'locations#index' #this is to configure it to go to the page when it is at the route
   get 'home' => 'home#welcome' #when we right /home we want it to go to home conteroller and execute welcome function/action
 end
