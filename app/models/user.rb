49995d195ba392cae232f95c1997e90fe48a31c8
Kristian Ruiz Kyvik <kristianruizkyvik@gmail.com>
2014-07-24 15:18:22 +0200

    Added tests to validation of model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,5 @@
 class User < ActiveRecord::Base
   has_many :visits
-  validates :username, presence: true, format: {with: /\A+[a-zA-Z0-9]+\z/}
+  validates :user_name, presence: true, format: {with: /\A+[a-zA-Z0-9]+\z/}
   validates :name, presence: true
-
-
-
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "fails with unvalid username (with space)" do
+    user=User.new(user_name: "jdk ds", name: "ben")
+    expect(user.valid?).to eq(false)
+  end
+
+  it "fails with unvalid username (with symbol)" do
+    user=User.new(user_name: "jdk$ds", name: "ben")
+    expect(user.valid?).to eq(false)
+  end
+
+  it "succeeds with valid username" do
+    user=User.new(user_name: "krist34", name: "ben")
+    expect(user.valid?).to eq(true)
+  end
+
 end
