e7d0325bb75d2c37354a41b7006412613bec7d72
Kristian Ruiz Kyvik <kristianruizkyvik@gmail.com>
2014-07-24 15:08:44 +0200

    added validation and association for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   has_many :visits
-  validates :username, presence: true
+  validates :username, presence: true, format: {with: /\A+[a-zA-Z0-9]+\z/}
   validates :name, presence: true
-  
+
+
 
 end
