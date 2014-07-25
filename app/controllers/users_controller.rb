8a1a65b7c4427ae698b9aa72e7647a5ed4ef662a
benja83 <tarennebenjamin@gmail.com>
2014-07-25 15:37:17 +0200

    added edit and update method in user controler

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,26 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @visits = Visit.where(:user_id => params[:id],:from_date => DateTime.now.beginning_of_month..DateTime.now.end_of_month)
   end
+
+  def edit
+  	@user = User.find(params[:id])
+  	render 'edit'
+  end
+
+  def update
+  	@user = User.find(params[:id])
+  			
+  	@user.update_attributes user_params
+  	if @user.save
+  	    redirect_to( action: 'index')
+  	else
+  	    render 'edit'
+  	end
+  end
+
+  private
+
+  def user_params
+  	params.require(:user_name).permit(:name, :nationality)
+  end
 end
